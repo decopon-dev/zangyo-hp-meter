1a061c1236b5ce96ca6c5edb8975cbdd8d6f0216
decopon <214833207+decopon-dev@users.noreply.github.com>
2026-09-12 15:47:49 +0900

    Create initial overtime HP meter page

---
+++ src/main/java/com/decopon/zangyohpmeter/ZangyoHpMeterApplication.java
@@ -0,0 +1,13 @@
+package com.decopon.zangyohpmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZangyoHpMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZangyoHpMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decopon/zangyohpmeter/ZangyoHpMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decopon.zangyohpmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZangyoHpMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
